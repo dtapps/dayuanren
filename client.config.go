d245c8813db591f8d2bf9792e71ca892b20ed7c4
dtapps <li.gc@icloud.com>
2024-06-12 16:31:53 +0800

    - update

--- client.config.go
+++ client.config.go
@@ -9,15 +9,23 @@ func (c *Client) ConfigClient(config *ClientConfig) {
 	c.config.apiKey = config.ApiKey
 }
 
+func (c *Client) SetUserID(userID int64) *Client {
+	c.config.userID = userID
+	return c
+}
+
+func (c *Client) SetApiKey(apiKey string) *Client {
+	c.config.apiKey = apiKey
+	return c
+}
+
 // SetClientIP 配置
-func (c *Client) SetClientIP(clientIP string) {
-	if clientIP == "" {
-		return
-	}
+func (c *Client) SetClientIP(clientIP string) *Client {
 	c.config.clientIP = clientIP
 	if c.httpClient != nil {
 		c.httpClient.SetClientIP(clientIP)
 	}
+	return c
 }
 
 // SetLogFun 设置日志记录函数
--- version.go
+++ version.go
@@ -1,5 +1,5 @@
 package dayuanren
 
 var (
-	Version = "1.0.11"
+	Version = "1.0.12"
 )
